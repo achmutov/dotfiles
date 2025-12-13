f98d7c557fda2a4a85425c528082de92b6212e83
Andrew Chmutov <achmutov@proton.me>
2025-12-13 17:27:38 +0100

    feat(nvim): add colorful-menu and lspkind for blink
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,10 @@
 return {
     "saghen/blink.cmp",
-    dependencies = "rafamadriz/friendly-snippets",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+        "xzbdmw/colorful-menu.nvim",
+        "onsails/lspkind.nvim",
+    },
     version = "*",
     opts = {
         -- "default" for mappings similar to built-in completion
@@ -17,6 +21,52 @@ return {
         completion = {
             documentation = { auto_show = true },
             ghost_text = { enabled = true },
+            menu = {
+                draw = {
+                    columns = { { "kind_icon" }, { "label", gap = 1 } },
+                    components = {
+                        kind_icon = {
+                            text = function(ctx)
+                                local icon = ctx.kind_icon
+                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                                    if dev_icon then
+                                        icon = dev_icon
+                                    end
+                                else
+                                    icon = require("lspkind").symbolic(ctx.kind, {
+                                        mode = "symbol",
+                                    })
+                                end
+
+                                return icon .. ctx.icon_gap
+                            end,
+
+                            -- Optionally, use the highlight groups from nvim-web-devicons
+                            -- You can also add the same function for `kind.highlight` if you want to
+                            -- keep the highlight groups in sync with the icons.
+                            highlight = function(ctx)
+                                local hl = ctx.kind_hl
+                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                                    if dev_icon then
+                                        hl = dev_hl
+                                    end
+                                end
+                                return hl
+                            end,
+                        },
+                        label = {
+                            text = function(ctx)
+                                return require("colorful-menu").blink_components_text(ctx)
+                            end,
+                            highlight = function(ctx)
+                                return require("colorful-menu").blink_components_highlight(ctx)
+                            end,
+                        },
+                    },
+                },
+            },
         },
 
         sources = {
