3277e388900840536dbaebc9a729496359d7557b
Andrew Chmutov <achmutov@proton.me>
2025-12-14 12:59:18 +0100

    feat(nvim, neogit): on directory open if inside worktree
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- nvim/.config/nvim/lua/config/neo-tree.lua
+++ nvim/.config/nvim/lua/config/neo-tree.lua
@@ -1,14 +1,18 @@
 vim.api.nvim_create_autocmd("BufEnter", {
-    -- make a group to be able to delete it later
-    group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
     callback = function()
         local f = vim.fn.expand("%:p")
         if vim.fn.isdirectory(f) ~= 0 then
-            vim.cmd("Neotree current dir=" .. f)
-            -- neo-tree is loaded now, delete the init autocmd
-            vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+            local git = require("neogit.lib.git")
+            local cmd
+            if git.cli.is_inside_worktree(f) then
+                cmd = "Neogit"
+            else
+                cmd = "Neotree current dir=" .. f
+            end
+            vim.cmd(cmd)
         end
     end,
+    once = true,
 })
 
 require("neo-tree").setup({
