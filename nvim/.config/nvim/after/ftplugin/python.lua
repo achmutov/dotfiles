ccc749a5c449902041e3373a034897d759154b0f
Andrew Chmutov <achmutov@proton.me>
2026-03-12 10:16:17 +0100

    fix(nvim): python indent, AGAIN
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- nvim/.config/nvim/after/ftplugin/python.lua
+++ nvim/.config/nvim/after/ftplugin/python.lua
@@ -1,4 +1,8 @@
 vim.g.python_indent = {
-    open_paren = vim.bo.shiftwidth,
+    disable_parentheses_indenting = false,
     closed_paren_align_last_line = false,
+    searchpair_timeout = 150,
+    continue = vim.bo.shiftwidth,
+    open_paren = vim.bo.shiftwidth,
+    nested_paren = vim.bo.shiftwidth,
 }
