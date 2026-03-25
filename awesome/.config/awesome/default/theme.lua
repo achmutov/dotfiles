59b321b3c79944693a8bf7c33c4863edee3ca937
Andrew Chmutov <achmutov@proton.me>
2026-03-25 15:26:43 +0100

    style(alacritty; awesome) go darker
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- awesome/.config/awesome/default/theme.lua
+++ awesome/.config/awesome/default/theme.lua
@@ -8,8 +8,8 @@ local theme = {}
 
 theme.font = "sans 8"
 
-theme.bg_normal = "#222222"
-theme.bg_focus = "#535d6c"
+theme.bg_normal = "#000000"
+theme.bg_focus = "#222222"
 theme.bg_urgent = "#ff0000"
 theme.bg_minimize = "#444444"
 theme.bg_systray = theme.bg_normal
@@ -21,7 +21,7 @@ theme.fg_minimize = "#ffffff"
 
 theme.border_width = dpi(1)
 theme.border_normal = "#000000"
-theme.border_focus = "#535d6c"
+theme.border_focus = "#6F5282"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
@@ -89,7 +89,7 @@ theme.titlebar_maximized_button_focus_inactive = themes_path .. "default/titleba
 theme.titlebar_maximized_button_normal_active = themes_path .. "default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active = themes_path .. "default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path .. "default/background.png"
+theme.wallpaper = themes_path .. "default/background.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
