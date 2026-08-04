7007a07ba01013fcad4d82ba42a4fb37b3a39a23
Andrew Chmutov <achmutov@proton.me>
2026-08-04 14:14:46 +0200

    fix(nvim, lsp): partially switch to system dev tooling
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -1,34 +1,48 @@
-local auto = "__auto"
+local T = {
+  none = "__none",
+  auto = "__auto",
+  lsp = vim.F.if_nil,
+}
+
 local mason2lsp = {
-  ["clangd"] = auto,
-  ["codelldb"] = nil,
-  ["css-lsp"] = "cssls",
-  ["debugpy"] = nil,
-  ["docker-compose-language-service"] = "docker_compose_language_service",
-  ["docker-language-server"] = "docker_language_server",
-  ["emmylua_ls"] = auto,
-  ["eslint-lsp"] = "eslint",
-  ["gh-actions-language-server"] = "gh_actions_ls",
-  ["gopls"] = auto,
-  ["json-lsp"] = "jsonls",
-  ["just-lsp"] = "just",
-  ["neocmakelsp"] = "neocmake",
-  ["oxfmt"] = auto,
-  ["oxlint"] = auto,
-  ["ruff"] = auto,
-  ["rust-analyzer"] = "rust_analyzer",
-  ["stylua"] = auto,
-  ["tailwindcss-language-server"] = "tailwindcss",
-  ["tsgo"] = auto,
-  ["ty"] = auto,
-  ["typos-lsp"] = "typos_lsp",
-  ["vue-language-server"] = "vue_ls",
-  ["yaml-language-server"] = "yamlls",
-  ["zls"] = auto,
+  -- npm
+  ["bash-language-server"] = T.lsp("bashls"),
+  ["css-lsp"] = T.lsp("cssls"),
+  ["docker-compose-language-service"] = T.lsp("docker_compose_language_service"),
+  ["docker-language-server"] = T.lsp("docker_language_server"),
+  ["eslint-lsp"] = T.lsp("eslint"),
+  ["gh-actions-language-server"] = T.lsp("gh_actions_ls"),
+  ["json-lsp"] = T.lsp("jsonls"),
+  ["oxfmt"] = T.auto,
+  ["oxlint"] = T.auto,
+  ["tailwindcss-language-server"] = T.lsp("tailwindcss"),
+  ["tsgo"] = T.auto,
+  ["vue-language-server"] = T.lsp("vue_ls"),
+  ["yaml-language-server"] = T.lsp("yamlls"),
+
+  -- github
+  ["clangd"] = T.auto,
+  ["codelldb"] = T.none,
+  ["shellcheck"] = T.none,
+  ["shfmt"] = T.none,
 }
 -- ["lua-language-server"] = "lua_ls",
 -- ["pyright"] = SAME,
 -- ["typescript-language-server"] = "ts_ls",
+
+local system_lsp = {
+  "emmylua_ls",
+  "gopls",
+  "just",
+  "neocmake",
+  "ruff",
+  "rust_analyzer",
+  "stylua",
+  "taplo",
+  "ty",
+  "typos_lsp",
+  "zls",
+}
 
 local treesitter = {
   "asm",
@@ -86,15 +100,21 @@ local treesitter = {
 }
 
 local mason = vim.tbl_keys(mason2lsp)
-local lsp = vim
+local mason_lsp = vim
   .iter(pairs(mason2lsp))
   :map(function(mason_name, lsp_name)
-    return lsp_name == auto and mason_name or lsp_name
+    if lsp_name == T.auto then
+      return mason_name
+    elseif lsp_name == T.none then
+      return nil
+    else
+      return lsp_name
+    end
   end)
   :totable()
 
 return {
   mason = mason,
-  lsp = lsp,
+  lsp = vim.list_extend(mason_lsp, system_lsp),
   treesitter = treesitter,
 }
