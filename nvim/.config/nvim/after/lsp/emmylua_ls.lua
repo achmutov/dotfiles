1af6e6a8c0f5af57460a746118bba6bd95362485
Andrew Chmutov <achmutov@proton.me>
2026-08-13 18:44:51 +0200

    chore(nvim, lsp): update lspconfig
    Signed-off-by: Andrew Chmutov <achmutov@proton.me>

---
+++ nvim/.config/nvim/after/lsp/emmylua_ls.lua
@@ -0,0 +1,10 @@
+return {
+  -- see: https://github.com/EmmyLuaLs/emmylua-analyzer-rust/blob/main/docs/config/emmyrc_json_EN.md
+  settings = {
+    emmylua = vim.NIL,
+    Lua = {
+      codeLens = { enable = true },
+      hint = { enable = true },
+    },
+  },
+}
